dab752483e988b44a90d243d62a2e5b954f20c07
Luke Weiler <lukew25073@gmail.com>
2022-08-11 23:50:50 -0400

    Update models.go to match new schema.json

--- models.go
+++ models.go
@@ -41,6 +41,7 @@ type Resume struct {
 	Languages    []language    `json:"languages"`
 	Interests    []interest    `json:"interests"`
 	References   []reference   `json:"references"`
+	Projects     []project     `json:"projects"`
 }
 
 type location struct {
@@ -60,10 +61,10 @@ type profile struct {
 type basics struct {
 	Name     string    `json:"name" validate:"required"`
 	Label    string    `json:"label" validate:"required"`
-	Picture  string    `json:"picture"`
+	Image	 string    `json:"image"`
 	Email    string    `json:"email" validate:"required,email"`
 	Phone    string    `json:"phone"`
-	Website  string    `json:"website" validate:"url"`
+	Url	 string    `json:"url" validate:"url"`
 	Summary  string    `json:"summary"`
 	Location location  `json:"location"`
 	Profiles []profile `json:"profiles"`
@@ -71,9 +72,9 @@ type basics struct {
 
 type work struct {
 	interval
-	Company    string   `json:"company"`
+	Name	   string   `json:"name"`
 	Position   string   `json:"position"`
-	Website    string   `json:"website" validate:"url"`
+	Url	   string   `json:"url" validate:"url"`
 	Summary    string   `json:"summary"`
 	Highlights []string `json:"highlights"`
 }
@@ -82,7 +83,7 @@ type volunteer struct {
 	interval
 	Organization string   `json:"organization"`
 	Position     string   `json:"position"`
-	Website      string   `json:"website" validate:"url"`
+	Url	     string   `json:"url" validate:"url"`
 	Summary      string   `json:"summary"`
 	Highlights   []string `json:"highlights"`
 }
@@ -90,9 +91,10 @@ type volunteer struct {
 type education struct {
 	interval
 	Institution string   `json:"institution"`
+	Url         string   `json:"url" validate:"url"`
 	Area        string   `json:"area"`
 	StudyType   string   `json:"studyType"`
-	Gpa         string   `json:"gpa"`
+	Score       string   `json:"score"`
 	Courses     []string `json:"courses"`
 }
 
@@ -103,11 +105,18 @@ type award struct {
 	Summary string   `json:"summary"`
 }
 
+type certificate struct {
+	Name	string	  `json:"name"`
+	Date	jsonTime  `json:"date" validate:"lte"`
+	Issuer	string    `json:"issuer"`
+        Url	string    `json:"url" validate:"url"`
+}
+
 type publication struct {
 	Name        string   `json:"name"`
 	Publisher   string   `json:"publisher"`
 	ReleaseDate jsonTime `json:"releaseDate" validate:"lte"`
-	Website     string   `json:"website" validate:"url"`
+	Url         string   `json:"url" validate:"url"`
 	Summary     string   `json:"summary"`
 }
 
@@ -130,4 +139,17 @@ type interest struct {
 type reference struct {
 	Name      string `json:"name"`
 	Reference string `json:"reference"`
+}
+
+type project struct {
+	Name		string		`json:"name"`
+	Description	string		`json:"description"`
+	Highlights	[]string	`json:"highlights"`
+	Keywords	[]string	`json:"keywords"`
+	StartDate	jsonTime	`json:"startDate" validate:"lte"`
+	EndDate		jsonTime	`json:"endDate" validate:"lte"`
+	Url		string		`json:"url" validate:"url"`
+	Roles		[]string	`json:"roles"`
+	Entity		string		`json:"entity"`
+	Type		string		`json:"type"`
 }
